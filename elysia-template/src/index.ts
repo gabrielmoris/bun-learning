254f95bae74ee32fe9fddb78dd3594771b692fca
gabriel <gabrielcmoris@gmail.com>
2023-09-16 14:08:02 +0200

    movies database connection with prisma and swagger docs

--- elysia-template/src/index.ts
+++ elysia-template/src/index.ts
@@ -1,33 +1,137 @@
+import { PrismaClient } from "@prisma/client";
 import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+
+// Way 1
+// const prisma = new PrismaClient();
+
+//way2
+const setup = (app: Elysia) => app.decorate("db", new PrismaClient());
 
 const app = new Elysia()
+  .use(swagger({ path: "/v1/swagger" }))
+  .use(setup)
   .group("/search", (app) => {
-    return app.guard(
-      {
-        query: t.Object({
-          q: t.String(),
-        }),
-      },
-      (app) =>
-        app
-          .get("/", ({ query }) => `query: ${query.q}`)
-          .get("/movie", ({ query }) => `query: ${query.q}`)
-          .get("/tv", ({ query }) => `query: ${query.q}`)
-          .get("/person", ({ query }) => `query: ${query.q}`)
-          .get("/company", ({ query }) => `query: ${query.q}`)
-          .get("/episode", ({ query }) => `query: ${query.q}`)
-          .get("/review", ({ query }) => `query: ${query.q}`)
-          .get("/award", ({ query }) => `query: ${query.q}`)
-    );
+    return app
+      .get("/", async ({ query, db }) => db.movie.findMany())
+      .guard(
+        {
+          query: t.Object({
+            q: t.String(),
+          }),
+        },
+        (app) =>
+          app
+            .get("/movie", async ({ query, db }) =>
+              db.movie.findMany({
+                where: {
+                  title: {
+                    contains: query.q,
+                  },
+                },
+              })
+            )
+            .get("/tv", async ({ query, db }) =>
+              db.movie.findMany({
+                where: {
+                  title: {
+                    contains: query.q,
+                  },
+                  type: "series",
+                },
+              })
+            )
+            .get("/person", async ({ query, db }) =>
+              db.person.findMany({
+                where: {
+                  name: {
+                    contains: query.q,
+                  },
+                },
+              })
+            )
+            // .get("/company",async ({ query, db }) =>
+            //   db.person.findMany({
+            //     where: {
+            //       company: {
+            //         contains: query.q,
+            //       },
+            //     },
+            //   })
+            // )
+            .get("/episode", async ({ query, db }) =>
+              db.episode.findMany({
+                where: {
+                  title: {
+                    contains: query.q,
+                  },
+                },
+              })
+            )
+            .get("/review", async ({ query, db }) =>
+              db.review.findMany({
+                where: {
+                  comment: {
+                    contains: query.q,
+                  },
+                },
+              })
+            )
+            .get("/award", async ({ query, db }) =>
+              db.award.findMany({
+                where: {
+                  name: {
+                    contains: query.q,
+                  },
+                },
+              })
+            )
+      );
   })
   .group("/title/:id", (app) => {
     return app
-      .get("/", ({ params }) => params.id)
-      .get("/episodes", ({ params }) => params.id)
-      .get("/cast", ({ params }) => params.id)
-      .get("/reviews", ({ params }) => params.id)
-      .get("/awards", ({ params }) => params.id);
+      .get("/", async ({ params, db }) =>
+        db.movie.findUnique({
+          where: {
+            id: parseInt(params.id, 10),
+          },
+        })
+      )
+      .get("/episodes", async ({ params, db }) =>
+        db.movie.findMany({
+          where: {
+            id: parseInt(params.id, 10),
+          },
+        })
+      )
+      .get("/cast", async ({ params, db }) =>
+        db.person.findMany({
+          where: {
+            movies: {
+              some: {
+                id: parseInt(params.id, 10),
+              },
+            },
+          },
+        })
+      )
+      .get("/reviews", async ({ params, db }) =>
+        db.review.findUnique({
+          where: {
+            id: parseInt(params.id, 10),
+          },
+        })
+      )
+      .get("/awards", async ({ params, db }) =>
+        db.award.findUnique({
+          where: {
+            id: parseInt(params.id, 10),
+          },
+        })
+      );
   })
   .listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}.
+Swagger docummentation in:
+http://${app.server?.hostname}:${app.server?.port}/v1/swagger`);
